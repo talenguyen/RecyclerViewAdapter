fb97aa7a6ef6064eebaf48ebd5883965f2b54d4f
tale <giangnguyen.tale@gmail.com>
2015-06-10 16:25:36 +0700

    demo app

--- app/src/main/java/com/tale/recyclerviewadapterdemo/MainActivityFragment.java
+++ app/src/main/java/com/tale/recyclerviewadapterdemo/MainActivityFragment.java
@@ -1,10 +1,17 @@
 package com.tale.recyclerviewadapterdemo;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.tale.recyclerviewadapter.DividerItemDecoration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -12,12 +19,30 @@ import android.view.ViewGroup;
  */
 public class MainActivityFragment extends Fragment {
 
+    private StringAdapter adapter;
+
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        final View view = inflater.inflate(R.layout.fragment_main, container, false);
+        final RecyclerView rv = (RecyclerView) view.findViewById(R.id.rv);
+        rv.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+        rv.setHasFixedSize(true);
+        rv.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL_LIST));
+        adapter = new StringAdapter();
+        rv.setAdapter(adapter);
+        adapter.changeDataSet(createTestString(20));
+        return view;
+    }
+
+    private List<String> createTestString(int size) {
+        final ArrayList<String> result = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            result.add("Item number " + i);
+        }
+        return result;
     }
 }
---
+++ app/src/main/java/com/tale/recyclerviewadapterdemo/StringAdapter.java
@@ -0,0 +1,22 @@
+package com.tale.recyclerviewadapterdemo;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tale.recyclerviewadapter.SimpleRecyclerViewAdapter;
+import com.tale.recyclerviewadapter.viewholder.BaseViewHolder;
+import com.tale.recyclerviewadapter.viewholder.Text1ViewHolder;
+
+/**
+ * Author tale. Created on 6/10/15.
+ */
+public class StringAdapter extends SimpleRecyclerViewAdapter<String> {
+
+    @Override
+    public BaseViewHolder<String> onCreateViewHolder(ViewGroup parent, int viewType) {
+        final LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        final View view = layoutInflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+        return new Text1ViewHolder(view, android.R.id.text1);
+    }
+}
